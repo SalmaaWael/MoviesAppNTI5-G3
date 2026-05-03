d8a7cb86bb26a032ff6dffa11f5f067267442561
Salma Wael <swaelghoneim@gmail.com>
2026-05-03 20:10:43 +0300

    Initial commit: Set up project environment and basic structure

---
+++ lib/core/colors_manager/colors_manager.dart
@@ -0,0 +1,8 @@
+import 'dart:ui' show Color;
+
+class ColorsManager{
+  static const Color background = Color(0xFF242A32);
+  static const Color unSelectedIcons = Color(0xFF67686D);
+  static const Color selectedIcons = Color(0xFF67686D);
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const WhiteScreen(),
+    );
+  }
+}
+
+class WhiteScreen extends StatelessWidget {
+  const WhiteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+    );
+  }
+}
