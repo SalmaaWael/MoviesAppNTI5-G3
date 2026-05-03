5e9d62d3e1ca59acbcb242b9e8b134c360ded987
AbdulazizAmer <abdulazizamer132@gmail.com>
2026-05-03 23:15:38 +0300

    zezo

--- lib/main.dart
+++ lib/main.dart
@@ -22,9 +22,6 @@ class WhiteScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: 
-        BackButtonIcon(key: Key(AutofillHints.creditCardExpirationDate),)
-      ,
     );
   }
 }
