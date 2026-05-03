44cb0c9017fc35515f0dd3e5f06b134df5ac62f2
Salma Wael <swaelghoneim@gmail.com>
2026-05-03 20:20:33 +0300

    Add AssetsManager and ColorsManager

---
+++ lib/core/assets_manager/assets_manager.dart
@@ -0,0 +1,16 @@
+class ImagesManager{
+  static const String base = "assets/images";
+  static const String splashScreen="$base/splash_screen_image.png";
+  static const String searchScreen="$base/search_screen_image.png";
+  static const String wishListScreen="$base/wishlist_screen_image.png";
+  static const String movie1="$base/movie_1.png";
+  static const String movie2="$base/movie_2.png";
+  static const String movie3="$base/movie_3.png";
+  static const String movie4="$base/movie_4.png";
+  static const String movie5="$base/movie_5.png";
+  static const String movie6="$base/movie_6.png";
+  static const String movie7="$base/movie_7.png";
+  static const String movie8="$base/movie_8.png";
+
+
+}
