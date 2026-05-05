1a2ade00760e725be3e3c3228dc9e0579cf2f623
AbdulazizAmer <abdulazizamer132@gmail.com>
2026-05-06 02:12:39 +0300

    feat: add home screen UI and connect popular movies API

---
+++ lib/api_manager/api_manager.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/movie_response.dart';
+
+class ApiManager {
+  static const String baseUrl = 'https://api.themoviedb.org/3';
+  static const String apiKey = '9d7f94be913eddf2db40e317d2f12f36';
+  static Future<MovieResponse> getPopularMovies() async {
+    var url = Uri.parse('$baseUrl/movie/popular?api_key=$apiKey');
+
+    try {
+      var response = await http.get(url);
+      if (response.statusCode == 200) {
+        var json = jsonDecode(response.body);
+
+        return MovieResponse.fromJson(json);
+      } else {
+        throw Exception('Failed to load movies. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Error fetching data: $e');
+    }
+  }
+}
--- lib/screens/home_screen/popular_movies_grid.dart
+++ lib/screens/home_screen/popular_movies_grid.dart
@@ -1,40 +1,67 @@
 import 'package:flutter/material.dart';
 
-import '../../core/assets_manager/assets_manager.dart';
+import '../../api_manager/api_manager.dart';
+import '../../models/movie_response.dart';
 
 class PopularMoviesGrid extends StatelessWidget {
   const PopularMoviesGrid({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
+    return FutureBuilder<MovieResponse>(
+      future: ApiManager.getPopularMovies(),
 
-      itemCount: 6,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
 
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
-        crossAxisSpacing: 8,
-        mainAxisSpacing: 8,
-        childAspectRatio: 0.7,
-      ),
-
-      itemBuilder: (context, index) {
-        return Padding(
-          padding: const EdgeInsets.all(2.0),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(12),
-            child: Container(
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(AssetsManager.movie1),
-                  fit: BoxFit.cover,
-                ),
-              ),
+        if (snapshot.hasError) {
+          return Center(
+            child: Text(
+              'Error: ${snapshot.error}',
+              style: const TextStyle(color: Colors.white),
             ),
-          ),
-        );
+          );
+        }
+
+        if (snapshot.hasData) {
+          var moviesList = snapshot.data?.results ?? [];
+
+          if (moviesList.isEmpty) {
+            return const Center(
+              child: Text('There is no movies now', style: TextStyle(color: Colors.white)),
+            );
+          }
+          return GridView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: moviesList.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              childAspectRatio: 0.7,
+            ),
+            itemBuilder: (context, index) {
+              var movie = moviesList[index];
+
+              return ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage('https://image.tmdb.org/t/p/w500${movie.posterPath}'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
+        }
+
+        return const SizedBox();
       },
     );
   }
